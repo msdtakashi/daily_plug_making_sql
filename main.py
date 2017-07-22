4255610b893346e4c9f6d05becf787411973e725
msdtakashi <msdtakashi@gmail.com>
2017-07-22 12:33:45 +0900

    SQLを追加

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-tstr = '2016-07-01'
+
+tstr = '2016-07-21'
 tdatetime = dt.strptime(tstr, '%Y-%m-%d')
-for i in range(60):
+for i in range(11):
     dd = tdatetime + datetime.timedelta(days=i)
-    print(dd.strftime('%Y')+'-'+dd.strftime('%m')+'-'+dd.strftime('%d'))
+    print("""
+    select 
+        uid
+    from
+    　intemidate.unique_user
+    where log_date = """,end='')
+    print("'"+dd.strftime('%Y')+"-"+dd.strftime('%m')+"-"+dd.strftime('%d')+"'")
