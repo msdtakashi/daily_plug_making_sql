2224796ec7b26422eebb3a48c03e65f511d48e07
msdtakashi <msdtakashi@gmail.com>
2017-07-24 01:23:54 +0900

    テスト用のデータ

---
+++ test_sql.sql
@@ -0,0 +1,11 @@
+create table duu_location (
+   log_date    date,
+   location     varchar(32),
+   aggegate_value integer,
+   create_date   date,
+   update_date   date
+);
+
+
+
+insert into  duu_location values ('2016-07-01', 'japan',10,'2016-07-01','2016-07-01' );
