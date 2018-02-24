8c16d2fedc94209be13d32c5ae0338a41719f57b
msdtakashi <msdtakashi@gmail.com>
2018-02-24 15:51:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-tstr = '2016-07-01'
+
+from datetime import datetime as dt
+
+#集計期間のスタート
+tstr = '2018-01-01'
+#集計期間を回す日数
+term = 10
+
+#読み込むファイルパス
+hoge = open('./Documents/python_path/test.sql','r')
+query_code = hoge.readlines()
+
 tdatetime = dt.strptime(tstr, '%Y-%m-%d')
-for i in range(60):
+
+for i in range(term):
     dd = tdatetime + datetime.timedelta(days=i)
-    print(dd.strftime('%Y')+'-'+dd.strftime('%m')+'-'+dd.strftime('%d'))
+    datesring= dd.strftime('%Y')+'-'+dd.strftime('%m')+'-'+dd.strftime('%d')
+    for line in query_code:
+        print(line.replace(":'target_date'", datesring),end="")
+    print("\n")
